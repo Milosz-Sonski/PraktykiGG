dbf8c357a19dcbc7db44f4637661b3f7ee51b82b
Milosz-Sonski <milosz.sonski@onet.pl>
2021-02-23 14:42:32 +0100

    Zadania 1-4

---
+++ Powitanie/src/HelloWord.java
@@ -0,0 +1,34 @@
+public class HelloWord {
+
+    public static void main(String[] args) {
+
+        // Zadanie 1
+        int A = 7;
+        double B = 2.4;
+        char C = 'M';
+        String D = "Soński";
+        System.out.println(A+ " "+" " + B + " " + " " + C + " " + " " +D);
+        System.out.println(" ");
+
+        //Zadanie 2
+        int myIntA = 5*5;
+        int myIntB = 3*3;
+        double sum = myIntA + myIntB;
+        System.out.println("Wynik = " + sum);
+        System.out.println(" ");
+
+        //Zadanie 3
+        int myIntC = 7*7;
+        double sqrt = Math.sqrt(3);
+        double myDoubleA = 49 * sqrt;
+        sum = myDoubleA/4;
+        System.out.println(sum);
+        System.out.println(" ");
+
+        //Zadanie 4
+        String myStringA = "Miłosz\t";
+        String myStringB = "Soński";
+        System.out.println(myStringA + myStringB);
+
+    }
+}
