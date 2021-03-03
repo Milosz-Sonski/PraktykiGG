9bd16ce0b0c9f7fc583485cfbbd09c8701cc8178
Milosz-Sonski <milosz.sonski@onet.pl>
2021-03-03 13:17:38 +0100

    Dziedziczenie

---
+++ Dziedziczenie/src/main.java
@@ -0,0 +1,26 @@
+public class main
+{
+    //Zadanie 1
+    public static void main(String[] args)
+    {
+        Kwadrat kwadrat = new Kwadrat(4);
+        System.out.println(kwadrat.obliczPole());
+
+        Prostokat prostokat = new Prostokat(4,2);
+        System.out.println(prostokat.poleProstokata());
+
+        //Zadanie 2
+        System.out.println();
+        Pies pies = new Pies(4,4,"Azor","czarna",12 );
+        pies.dajGłos();
+
+        //Zadanie 3
+        System.out.println();
+        Spawacz spawacz = new Spawacz("niebezpieczna", 1525.25, "cały");
+        spawacz.wykonujePrace();
+
+        //Zadanie 4
+        //a. Odp: Metoda druga przysłania metodę trzecią.
+        //b. Odp: Niewidzialna jest metoda numer trzy.
+    }
+}
